0606a4dca170a5639c74a702292ea037dee7ead3
Liuba Kuibida <k2.liuba@gmail.com>
2024-04-26 17:06:16 +0200

    init commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HomeView from './views/HomeView.vue';
+import StartView from '@/views/StartView.vue';
+</script>
+
+<template>
+  <HomeView />
+  <StartView />
+</template>
---
+++ src/views/StartView.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>My old friend</h2>
+</template>
