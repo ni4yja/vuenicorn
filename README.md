5a6beb836077f5bb88e628461a6b4c3f637ee6eb
Liuba Kuibida <k2.liuba@gmail.com>
2024-04-28 22:56:33 +0200

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# Vuenicorn 🦄
-
-### Vue 3 Learning project for [BE Mentoring Program](https://inscience.io/en/be/) 2024
--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import HomeView from './views/HomeView.vue';
-import StartView from '@/views/StartView.vue';
-</script>
-
-<template>
-  <HomeView />
-  <StartView />
-</template>
